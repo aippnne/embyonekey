97635017f5c9269d3cf65806f6c378c108ee8a36
aippnne <33364159+aippnne@users.noreply.github.com>
2021-02-14 00:15:41 +0800

    Update embyonekey.sh

--- embyonekey.sh
+++ embyonekey.sh
@@ -2,7 +2,7 @@
 cd /var/packages/EmbyServer/target/mono/bin
 curl https://raw.githubusercontent.com/aippnne/embyonekey/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 ./cert-sync /etc/ssl/certs/ca-certificates.crt
-cd /usr/local/etc/nginx/conf.d/9b390d8c-ddf3-4bd9-b757-f2715cb02df0
+cd /usr/local/etc/nginx/conf.d/*-*-*-*
 wget https://raw.githubusercontent.com/aippnne/embyonekey/master/user.conf
 nginx -s reload
 
